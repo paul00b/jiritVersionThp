93f632dc87510dc64379ed7cf18fcccffb0e1785
Paul <broussolle.paul@gmail.com>
2018-05-05 18:48:43 +0200

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+   before_action :sanitize_devise_params, only: [:create]
+
+
+   def sanitize_devise_params
+       devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+   end
+
+
 end
