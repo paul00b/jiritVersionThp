8e5111eb7c4b60944708f4eb61d49d06c61b465c
Paul <broussolle.paul@gmail.com>
2018-05-07 21:10:27 +0200

    first

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :views
   get 'my_facts', to: 'facts#my_facts', as: 'myfacts'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
